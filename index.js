b1301894c88f9ed580f0cccaa3cf8dc069ab4830
Matthew <mattj20@gmail.com>
2022-10-25 17:02:34 -0700

    commit tree 6f9701a4b01d6cda370fa4ff4365cff97c800279

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+function mapToNegativize(array) {
+    return array.map(x => x*-1)
+}
+
+function mapToNoChange(array) {
+    return array.map(x => x)
+}
+
+function mapToDouble(array) {
+    return array.map(x => x*2)
+}
+
+function mapToSquare(array) {
+    return array.map(x => x**2)
+}
+
+function reduceToTotal(array, startingPoint = 0) {
+    return array.reduce((accum, curr) => accum + curr, startingPoint)
+}
+function reduceToAllTrue(array) {
+    let test = array.reduce(function(accum, curr) {
+        if (accum[`${!!curr}`]) {
+            accum[`${!!curr}`]++
+        }
+        else {
+            accum[`${!!curr}`] = 1
+        }
+        return accum
+   }, {})
+   if (!!test.false) {
+    return false
+   }
+   else {
+    return true
+   }
+}
+
+function reduceToAnyTrue(array) {
+    let test = array.reduce(function(accum, curr) {
+        if (accum[`${!!curr}`]) {
+            accum[`${!!curr}`]++
+        }
+        else {
+            accum[`${!!curr}`] = 1
+        }
+        return accum
+    }, {})
+    if (!!test.true) {
+        return true
+    }
+    else {
+        return false
+    }
+}
